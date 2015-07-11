1931113f3f98df1a2075702a748f5d64f2207cf0
ryanbmarx <ryanbmarx@gmail.com>
2015-07-11 17:14:24 -0500

    change beta bucket

--- tarbell_config.py
+++ tarbell_config.py
@@ -34,7 +34,8 @@ S3_BUCKETS = {
     # then use tarbell publish mytarget to publish to it
     
     "production": "graphics.chicagotribune.com/eastland",
-    "staging": "apps.beta.tribapps.com/eastland",
+    "staging": "graphics.beta.tribapps.com/eastland",
+    "secret": "graphics.chicagotribune.com/KcAyEKoeYTDjWCcybA8FNeWJhyzs"
 }
 
 # Default template variables
@@ -50,3 +51,28 @@ DEFAULT_CONTEXT = {
             'type': 'dataproject',
         },
 }
+
+# Add some filters
+from flask import Blueprint
+blueprint = Blueprint('noble-charter-discipline', __name__)
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US')
+
+@blueprint.app_template_filter('roundme')
+def roundme(text):
+   text=int(text)
+   return text
+
+   
+@blueprint.app_template_filter('add_suffix')
+def add_suffix(text):
+  text = str(text)
+  if text.endswith("1"):
+    text = text + "st"
+  elif text.endswith("2"):
+    text = text + "nd"
+  elif text.endswith("3"): 
+    text = text + "rd"
+  else:
+    text = text + "th"
+  return text 
